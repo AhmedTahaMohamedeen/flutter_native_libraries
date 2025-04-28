58139b9943fa3e20240022bdd8d427ce92275725
Ahmed Taha <81632866+AhmedTahaMohamedeen@users.noreply.github.com>
2025-04-28 13:05:57 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        ndkVersion = "28.0.12916984"
     }
 
     buildTypes {
